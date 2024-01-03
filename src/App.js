00923ee9a08987ff3d9fd3eed20bc2c98386b44e
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-01-03 12:56:39 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './Home'
 import Home2 from './Home2'
+import { Modal } from 'react-bootstrap'
+import Modals from './Component/Modals'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
     <div>
+      {/* <Modals/> */}
 <Home/>
 {/* <Home2/> */}
 
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
+import Modals from './Component/Modals';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
 
 const Home = () => {
   // 1 Api call
@@ -7,10 +10,11 @@ const Home = () => {
   // 3  map on state
   const [storeData, setStoreData] = useState();
   const [search,setSearch] = useState("people");
+  const [page,setPage] = useState(1)
   // const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
   let apiKey = 'c19t5LBUeXItCyO5nb5bpds50rtTKwmX7xOeFa4PnEBN8jabAxpjQW6U';
   // let apiUrl = `https://api.pexels.com/v1/search?query=${search}`;
-  let apiUrl = `https://api.pexels.com/v1/search?query=${search}&per_page=20`;
+  let apiUrl = `https://api.pexels.com/v1/search?query=${search}&per_page=20&page=${page}`;
   const searchButtonHandler =()=>{
     fetch(apiUrl, {
       method: 'GET',
@@ -26,15 +30,20 @@ const Home = () => {
     }
       useEffect(() => {
         searchButtonHandler()
-  }, []);
+  }, [page]);
 
-
-
-
+  const prevButtonHandle =()=>{
+setPage(page-1)
+  }
+  const nextButtonHandle =()=>{
+    setPage(page+1)   
+  }
 
   return (
-    <>
-  <div className='search_wrapper'>
+    <div>
+  <Modals/>
+
+<div className='search_wrapper'>
 <input type='text' onChange={(e)=>setSearch(e.target.value)}/>
 <button onClick={searchButtonHandler}>Search</button>
 
@@ -43,11 +52,16 @@ const Home = () => {
       <div className="mainBox_wrapImg">
         {storeData && storeData.map((val) =>
         //  console.log(val)
-         <img src={val.src.landscape} className='imgIndividual'/>
+         <img src={val.src.small} className='imgIndividual'/>
          )
          }
+    
       </div>
-    </>
+      <div className='text-center'> 
+      <button onClick={prevButtonHandle}>Prev</button>
+         <button onClick={nextButtonHandle}>Next</button>
+      </div>
+    </div>
   );
 };
 
